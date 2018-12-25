bdffec7eab31e4fd7d1e7617738b589550cabd58
Franz Louis Cesista <franzlouiscesista@gmail.com>
2018-12-26 02:41:06 +0800

    fixed bug in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 codeball2018-linux/codeball2018 --p1-name CUR-STRAT --p2-name PREV-STRAT --p2 tcp-31002 --noshow --results-file res.txt --duration 7200 &
 # codeball2018-linux/codeball2018 --p1-name CUR-STRAT --p2-name PREV-STRAT --p2 tcp-31002 --results-file res.txt &
-sleep 1; cpp-cgdk/build/MyStrategy &
-sleep 1; cpp-cgdk/build/MyPrevStrategy 127.0.0.1 31002 0000000000000000
+sleep 1; cpp-cgdk/versions/MyStrategy_v8_beta &
+sleep 1; cpp-cgdk/versions/MyStrategy_v7 127.0.0.1 31002 0000000000000000
 
 wait
 echo DONE!
