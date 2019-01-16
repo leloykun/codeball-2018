5bd9fb5c2ddbb209edadbe7bccd453da2c9e45eb
Franz Louis Cesista <franzlouiscesista@gmail.com>
2019-01-17 05:06:02 +0800

    version 34

--- run_multi.sh
+++ run_multi.sh
@@ -16,6 +16,7 @@ for ((batch = 0; batch < $BATCHES; batch++)) do
       let port_1=3100+$a
       let port_2=3100+$b
       # echo $batch $core $game $a $b $port_1 $port_2
+      # printf "1:1:OK\n2:0:OK\n7186238716238\n" > codeball2018-linux/result_$game.txt
       codeball2018-linux/codeball2018 \
         --duration $DURATION \
         --p1-name V$VERSION_P1 --p1 tcp-$port_1 \
@@ -30,67 +31,17 @@ for ((batch = 0; batch < $BATCHES; batch++)) do
   done
 
 echo "RESULTS:"
+let P1_wins=0
+let P2_wins=0
 for ((game = 0; game < $((BATCHES*NUM_CORES)); game++))
   do
-    cat codeball2018-linux/result_$game.txt
+    readarray -t RESFILE < codeball2018-linux/result_$game.txt
+    if ((${RESFILE[0]:0:1} == "1"))
+    then
+      let P1_wins=$P1_wins+1
+    else
+      let P2_wins=$P2_wins+1
+    fi
   done
-: '
-for ((i = 1; i <= $NUM_GAMES; i++))
-  do
-    let a=2*$i-1
-    let b=2*$i
-    echo $i $a $b
-    codeball2018-linux/codeball2018 \
-      --duration $DURATION \
-      --p1-name V$VERSION_P1 --p1 tcp-3100$a \
-      --p2-name V$VERSION_P2 --p2 tcp-3100$b \
-      --results-file result_$i.txt --no-countdown \
-      --noshow --log-file game$i.log --nitro true &
-    sleep 1; cpp-cgdk/versions/MyStrategy_v$VERSION_P1 127.0.0.1 3100$a $a &
-    sleep 1; cpp-cgdk/versions/MyStrategy_v$VERSION_P2 127.0.0.1 3100$b $b &
-  done
-
-wait
-echo "DONE!"
-
-echo "RESULTS:"
-for ((i = 1; i <= $NUM_GAMES; i++))
-  do
-    cat codeball2018-linux/result_$i.txt
-  done
-'
-: '
-codeball2018-linux/codeball2018 --p1-name CUR-STRAT --p1 tcp-31001 \
-  --p2-name PREV-STRAT --p2 tcp-31002 --results-file res1.txt --no-countdown \
-  --noshow --log-file game1.log --nitro true &
-sleep 1; cpp-cgdk/versions/MyStrategy_v33 127.0.0.1 31001 1 &
-sleep 1; cpp-cgdk/versions/MyStrategy_v34 127.0.0.1 31002 2 &
-
-codeball2018-linux/codeball2018 --p1-name CUR-STRAT --p1 tcp-31003 \
-  --p2-name PREV-STRAT --p2 tcp-31004 --results-file res2.txt --no-countdown \
-  --noshow --log-file game2.log --nitro true &
-sleep 1; cpp-cgdk/versions/MyStrategy_v33 127.0.0.1 31003 3 &
-sleep 1; cpp-cgdk/versions/MyStrategy_v34 127.0.0.1 31004 4 &
-
-codeball2018-linux/codeball2018 --p1-name CUR-STRAT --p1 tcp-31005 \
-  --p2-name PREV-STRAT --p2 tcp-31006 --results-file res3.txt --no-countdown \
-  --noshow --log-file game3.log --nitro true &
-sleep 1; cpp-cgdk/versions/MyStrategy_v33 127.0.0.1 31005 5 &
-sleep 1; cpp-cgdk/versions/MyStrategy_v34 127.0.0.1 31006 6 &
-
-codeball2018-linux/codeball2018 --p1-name CUR-STRAT --p1 tcp-31007 \
-  --p2-name PREV-STRAT --p2 tcp-31008 --results-file res4.txt --no-countdown \
-  --noshow --log-file game4.log --nitro true &
-sleep 1; cpp-cgdk/versions/MyStrategy_v33 127.0.0.1 31007 7 &
-sleep 1; cpp-cgdk/versions/MyStrategy_v34 127.0.0.1 31008 8
-
-wait
-echo DONE!
-
-echo RESULTS:
-cat codeball2018-linux/res1.txt
-cat codeball2018-linux/res2.txt
-cat codeball2018-linux/res3.txt
-cat codeball2018-linux/res4.txt
-
-'
+echo "VERSION $VERSION_P1 WINS: $P1_wins"
+echo "VERSION $VERSION_P2 WINS: $P2_wins"
